4851b9661a9c8bd407fef72cfdce20d56cd23ce9
Geoff Low <glow@mdsol.com>
2016-11-28 11:47:54 +0000

    coverage 100% refaxctored module

---
+++ flask_mauth/rsa_public_decrypt/rsa_decrypt.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'glow'
+
+from rsa import PublicKey, common, transform, core
+import six
+import base64
+
+
+class RSAPublicKey(PublicKey):
+
+    def public_decrypt(self, message):
+        """
+        Decrypt a String encrypted with a private key, returns the hash
+        :param message: encrypted message
+        :return:
+        """
+        # base64 decode
+        decoded = base64.b64decode(six.b(message))
+        # transform the decoded message to int
+        encrypted = transform.bytes2int(decoded)
+        """:type : int"""
+        payload = core.decrypt_int(encrypted, self.e, self.n)
+        """:type : int"""
+        padded = transform.int2bytes(payload, common.byte_size(self.n))
+        """:type : str"""
+        return padded
+
+    def unpad_message(self, padded):
+        """
+        Removes the padding from the string
+        :param padded: padded string
+        :rtype: str
+        """
+        return padded[padded.index(b'\x00', 2) + 1:]
